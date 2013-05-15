1818bf92c39f0e0d2cdf603b66d4b506b0f8de13
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-14 20:39:07 -0500

    working version with create, edit functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -20,7 +20,44 @@ get '/post' do
   erb :post
 end
 
-get '/post_something' do
+get '/create_post' do
+  @categories = Category.all
   erb :create_post
 end
 
+get '/edit_post' do
+  found_posts = Post.where(:edit_key => params[:id])
+  unless found_posts.empty?
+    @post = found_posts.first 
+    @id = @post.id
+  end
+
+  @categories = Category.all
+
+  erb :edit_post
+end
+
+post '/post_created' do
+
+  title = params[:title]
+  description = params[:description]
+  location = params[:location]
+  price = params[:price]
+  category_id = params[:category_id]
+  id = params[:id]
+
+  if id == '0'
+    @edit_key = params[:title].hash.to_s(36)
+
+    Post.create!(:title => title, :description => description, :location => location, 
+      :price => price, :category_id => category_id, 
+      :edit_key => @edit_key )
+
+  else 
+    Post.update(id, :title => title, :description => description, 
+      :location => location, :price => price, :category_id => category_id)
+    @edit_key = Post.where(:id => id).first[:edit_key]
+  end
+
+  erb :post_created
+end
